2ca79a7b6a33832d88cb53cb66fe492e7d7b57ed
zacharybaca <zachary.baca@msn.com>
2024-05-14 16:20:59 -0500

    added instructions

---
+++ advanced-looping-techniques.py
@@ -0,0 +1,15 @@
+# Our playlist of genres
+genres = ['Jazz', 'Rock', 'Hip-hop', 'Classical']
+
+"""Task 1: The Selective DJ
+Loop through a slice of the genres list from the previous question and print out the genres. Use slicing to create a sublist of genres from the second to the fourth track."""
+
+
+"""Task 2: The One-Liner Band with List Comprehensions
+Use a list comprehension to create a new list that contains each genre with the word "Music" appended to it. Print this new list."""
+
+
+"""Task 3: Numerical Beats with range
+Write a loop using range() to print out a countdown from 10 to 1, followed by the message "The beat drops now!"."""
+
+
